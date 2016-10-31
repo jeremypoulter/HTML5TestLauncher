048957be623c48d533bb1cd5791002d19b96675f
Jeremy Poulter <jeremy@bigjungle.net>
2016-10-31 12:10:39 +0000

    Added menu options to load the different test runners

--- mobile/src/main/java/org/dlna/html5testlauncher/MainActivity.java
+++ mobile/src/main/java/org/dlna/html5testlauncher/MainActivity.java
@@ -14,35 +14,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final private String TEST_RUNNER_URL = "http://web-platform.test:8000/tools/runner/tests.html";
+    static final private String LOCAL_FULL_TEST_RUNNER_URL = "http://web-platform.test:8000/tools/runner/index.html";
+    static final private String LOCAL_SIMPLE_TEST_RUNNER_URL = "http://web-platform.test:8000/tools/runner/tests.html";
+    static final private String W3C_TEST_RUNNER_URL = "http://w3c-test.org/tools/runner/index.html";
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView)findViewById(R.id.web_view);
+        webView = (WebView)findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new HtmlTestWebViewClient());
-        webView.loadUrl(TEST_RUNNER_URL);
-
-        /*
-        Button button = (Button)findViewById(R.id.system_browser_button);
-        if(null != button) {
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    try {
-                        Uri uri = Uri.parse(TEST_RUNNER_URL);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
-                    } catch (Exception e) {
-                        Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-        */
+        webView.loadUrl(LOCAL_SIMPLE_TEST_RUNNER_URL);
     }
 
     @Override
@@ -58,12 +44,21 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.system_browser:
                 try {
-                    Uri uri = Uri.parse(TEST_RUNNER_URL);
+                    Uri uri = Uri.parse(webView.getUrl());
                     Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(intent);
                 } catch (Exception e) {
                     Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
+                break;
+            case R.id.w3c_runner:
+                webView.loadUrl(W3C_TEST_RUNNER_URL);
+                break;
+            case R.id.local_full_runner:
+                webView.loadUrl(LOCAL_FULL_TEST_RUNNER_URL);
+                break;
+            case R.id.local_simple_runner:
+                webView.loadUrl(LOCAL_SIMPLE_TEST_RUNNER_URL);
                 break;
             default:
                 return super.onOptionsItemSelected(item);
